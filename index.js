10fefa94ccb918dec6fb6c723b36d18e4dc9c9a0
Mariia <m.goncharenkoo@icloud.com>
2022-10-09 21:21:12 -0400

    commit tree 37949a3688dc917ce72552c800d2292ff0f8f513

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+]
+
+// function superbowlWin(element) {
+//     return element.result === "W"
+// }
+
+
+function superbowlWin(record) {
+    const findWinner = record.find((element) => element.result === "W")
+    if (typeof findWinner === "object") {
+        return findWinner.year
+    } else {
+        return undefined
+    }
+}
